f3451327637ae3ac602921faaa94a487b0965cd2
Flow-Pie <startabase@gmail.com>
2025-05-11 21:26:51 +0300

    ✅MINOR CHANGES: gitignore, .etc

--- API-Core/app/__init__.py
+++ API-Core/app/__init__.py
@@ -11,8 +11,11 @@ def create_app():
 
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('MYSQL_URL') #msql DBMS
+    #app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('NEON_POSTGRES_URL')
+    #app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('MYSQL_URL') #msql DBMS
     #app.config['SQLALCHEMY_DATABASE_URI'] =  os.getenv('SQLITE_URL')  #uncomment this if you prefer sqlite DBMS
+
+
 
     print("INFO [CREATE_APP] Connection to database successful")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
