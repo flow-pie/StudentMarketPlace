c8abdc362700a609951d388c49c4dbb68c154eb7
Flow-Pie <startabase@gmail.com>
2025-05-11 05:15:53 -0700

    ✅CLEANUP ISSUE #39 Setup flask sql alchemy and migrations Adhered to flask clean architecture model

--- API-Core/app/__init__.py
+++ API-Core/app/__init__.py
@@ -3,22 +3,31 @@
 from dotenv import load_dotenv
 from flask import Flask
 import os
-from .extensions import db, jwt, cors
+from .extensions import db, jwt, cors, migrate
 
 def create_app():
+    from . import models
     load_dotenv()
 
     app = Flask(__name__)
-    #app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('MYSQL_URL') #msql DBMS
-    app.config['SQLALCHEMY_DATABASE_URI'] =  os.getenv('SQLITE_URL')  #uncomment this if you prefer sqlite DBMS
-    print("[CREATE_APP] Connection to database successful")
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('MYSQL_URL') #msql DBMS
+    #app.config['SQLALCHEMY_DATABASE_URI'] =  os.getenv('SQLITE_URL')  #uncomment this if you prefer sqlite DBMS
+
+    print("INFO [CREATE_APP] Connection to database successful")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    db.init_app(app)#initializinf the app
+    #initialize extensions
+    db.init_app(app)
+    #--setup cors for frontend communication
     cors.init_app(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True)
+    #--perform db migration using app migrate obj
+    migrate.init_app(app, db)
+    #--jwt
     jwt.init_app(app)
 
-    with app.app_context():
-        db.create_all()
+    # with app.app_context(): #not needed when using flask migrate
+    #     db.create_all()
+
 
     return app
--- API-Core/app/extensions.py
+++ API-Core/app/extensions.py
@@ -1,8 +1,14 @@
 # Extensions init: db, jwt, cors
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 #db
 from flask_sqlalchemy import SQLAlchemy
 db= SQLAlchemy() #new sqlAlchemy object
+migrate = Migrate()
 cors= CORS()
 jwt = JWTManager()
+
+def init_app(app):
+    db.init_app(app)
+    migrate.init_app(app, db) #to initialize migrations with app and db
